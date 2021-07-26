f3437506ad6f85d3f9f4196df1eea778518724ed
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-26 18:47:38 +0430

    Added support for GET & POST routes in Router

--- core/Router.php
+++ core/Router.php
@@ -2,11 +2,26 @@
 
 namespace App\Core;
 
+use Closure;
+
 /**
  * Class Router
  * @package App\Core
  */
 class Router
 {
+    protected array $routes = [
+        'GET' => [],
+        'POST' => [],
+    ];
 
+    public function get(string $path, Closure $closure)
+    {
+        $this->routes['GET'][$path] = $closure;
+    }
+
+    public function post(string $path, Closure $closure)
+    {
+        $this->routes['POST'][$path] = $closure;
+    }
 }
