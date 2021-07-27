710876ec54d8a2f67e656d8480ceb89104d5d08e
sina shariati <sinashariati@sinas-MacBook-Pro.local>
2021-07-27 16:24:12 +0430

    Add exceptions class for custom exceptions

---
+++ exceptions/PageNotFoundException.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Exceptions;
+
+
+use Exception;
+use Throwable;
+
+class PageNotFoundException extends Exception
+{
+    /**
+     * InvalidPathException constructor.
+     */
+    public function __construct($message, $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
